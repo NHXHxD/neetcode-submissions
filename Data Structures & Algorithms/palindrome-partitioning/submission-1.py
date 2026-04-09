631e9341672b764ed636eafff4e9a79f78eb58de
Hy Nguyen <85931416+NHXHxD@users.noreply.github.com>
2026-04-10 01:26:34 +0930

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def check(ss):
+            l, r = 0, len(ss) - 1
+            while l < r:
+                if ss[l] != ss[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        curr = []
+        res = []
+        def dfs(start, path):
+            if start == len(s):
+                f = True
+                for n in path:
+                    if not check(n):
+                        f = False
+                if f:
+                    res.append(path.copy())
+                return
+            
+            for end in range(start + 1, len(s) + 1):
+                path.append(s[start:end])
+                dfs(end, path)
+                path.pop()
+        
+        dfs(0, [])
+        return res
